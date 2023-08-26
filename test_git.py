db02312d2e4cfbdf781931e861d2faade12cd639
Only Up <eohuseyn@gmail.com>
2023-08-26 13:11:57 +0400

    Hi

--- test_git.py
+++ test_git.py
@@ -1,1 +1,18 @@
-print("Hello World")
+import tkinter as tk
+
+def on_enter(event):
+    button.config(bg="yellow")
+
+def on_leave(event):
+    button.config(bg="SystemButtonFace")  # Reset to the default background color
+
+window = tk.Tk()
+window.title("Hover Effect Example")
+
+button = tk.Button(window, text="Hover Me")
+button.pack()
+
+button.bind("<Enter>", on_enter)
+button.bind("<Leave>", on_leave)
+
+window.mainloop()
